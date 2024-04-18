8000b9fad743243963b63688fa587e80e29fc3e3
Emran-Y <emranyonas602@gmail.com>
2024-04-19 00:09:57 +0300

    first func added

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,148 @@
-import React, { useCallback, useRef } from 'react';
+import { useCallback, useRef,useState,useEffect, CSSProperties } from 'react';
 import { toPng } from 'html-to-image';
 import './App.css'
+import { styleOne } from './styles';
+import { styleTwo } from './styles';
+import { styleThree } from './styles';
+import { styleFour } from './styles';
 
 function App() {
 
+
+
+  
+  const [curNumber, setCurNumber] = useState(1);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [imageUrl, setImageURL] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+
   const ref = useRef<HTMLDivElement>(null)
 
-  const onButtonClick = useCallback(() => {
-    if (ref.current === null) {
-      return
+  const onButtonClick = useCallback((cur: number) => {
+    setCurNumber(cur);
+    if (!ref.current) {
+      return;
     }
-
-    toPng(ref.current, { cacheBust: true, })
+  
+    toPng(ref.current, { cacheBust: true })
       .then((dataUrl) => {
-        const link = document.createElement('a')
-        link.download = 'my-image-name.png'
-        link.href = dataUrl
-        link.click()
+        const link = document.createElement('a');
+        link.download = `image-${cur}.png`;
+        link.href = dataUrl;
+        link.click();
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
+      });
+  }, [ref]);
+  
+
+
+  
+   
+    
+    const imageToBeGenerated1 = (
+      <div style={styleOne.bgContainer as CSSProperties} ref={ref}>
+        <img src={`/backgrounds/background1.png`} className='bg-image' alt='bg-image' />
+        <div style={styleOne.printContainer as CSSProperties}>
+          {/* <img src="prints/print1.jpg" className='print-image' alt="print Image" /> */}
+        { isUploaded && <img src={imageUrl} className='print-image' alt="print Image" />}
+        </div>
+      </div>
+    );
+
+    const imageToBeGenerated2 = (
+      <div style={styleTwo.bgContainer as CSSProperties} ref={ref}>
+        <div style={styleTwo.printContainer as CSSProperties}>
+          {/* <img src="prints/print1.jpg" className='print-image' alt="print Image" /> */}
+        {isUploaded && <img src={imageUrl} className='print-image' alt="print Image" />}
+        </div>
+      </div>
+    );  
+    
+    const imageToBeGenerated3 = (
+      <div style={styleThree.bgContainer as CSSProperties} ref={ref}>
+        <div style={styleThree.printContainer as CSSProperties}>
+          {/* <img src="prints/print1.jpg" className='print-image' alt="print Image" /> */}
+        {isUploaded && <img src={imageUrl} className='print-image' alt="print Image" />}
+        </div>
+      </div>
+    );
+    
+
+    const imageToBeGenerated4 = (
+      <div style={styleFour.bgContainer as CSSProperties} ref={ref}>
+        <div style={styleFour.printContainer as CSSProperties}>
+          {/* <img src="prints/print1.jpg" className='print-image' alt="print Image" /> */}
+        {isUploaded && <img src={imageUrl} className='print-image' alt="print Image" />}
+        </div>
+      </div>
+    );
+    
+    
+    
+    const handleFileChange = useCallback((image: File) => {
+      setIsUploading(true);
+      const data = new FormData();
+      data.append('file', image);
+      data.append('upload_preset', 'tohco7vu');
+  
+      fetch(`https://api.cloudinary.com/v1_1/difavbhph/image/upload`, {
+        method: 'post',
+        body: data,
       })
-  }, [ref])
+        .then((res) => res.json())
+        .then((data) => {
+          setImageURL(data.secure_url);
+          setIsUploaded(true);
+          setIsUploading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsUploading(false);
+        });
+    }, []);
 
-
-  const handleClick = () => {
-    console.log('I was clicked')
-  }
+ 
 
 
   return (
     <div className='container'>
-      <div className='my-image' ref={ref}>
-        <div className='my-frame'></div>
+       { isUploaded  ?  (curNumber === 1 ? imageToBeGenerated1 : curNumber === 2 ? imageToBeGenerated2 : curNumber === 3 ? imageToBeGenerated3 : imageToBeGenerated4): 
 
+       
+        <div>
+          the image will be here
+        </div>
+
+       }
+
+      
+
+      
+      { isUploaded &&
+        <div className='btns'>
+        <button onClick={() => onButtonClick(1)}>Download Image 1</button>
+        <button onClick={() => onButtonClick(2)}>Download Image 2</button>
+        <button onClick={() => onButtonClick(3)}>Download Image 3</button>
+        <button onClick={() => onButtonClick(4)}>Download Image 4</button>
       </div>
-      <button onClick={onButtonClick} >Download</button>
+      }
+
+      {
+      !isUploaded &&
+      <div className='my-input-form'>
+      <input
+          id='profile-pic'
+          type='file'
+          accept='image/*'
+          onChange={(e) => e.target.files && handleFileChange(e.target.files[0])}
+          className='fileInput'
+        />
+
+      {isUploading && <p>Uploading...</p>}
+      </div>
+      } 
 
     </div>
   )
---
+++ src/styles.tsx
@@ -0,0 +1,82 @@
+export const styleOne = {
+    bgContainer: {
+      width: "900px",
+      height: "800px",
+      position: "relative",
+    },
+    printContainer: {
+      zIndex: "999",
+      position: "absolute",
+      width: "335px",
+      height: "484px",
+      top: "167px",
+      left: "250px",
+    },
+};
+
+export const styleTwo = {
+    bgContainer: {
+        position: "relative",
+        width: "900px",
+        height: "600px",
+        zIndex: "0",
+        backgroundImage: "url(/backgrounds/background2.png)",
+        backgroundSize: "cover",
+        
+    },
+    printContainer: {
+        width: "124px",
+        height: "155px",
+        zIndex: "999",
+        position: "absolute",
+        top: "195px",
+        left: "549px",
+        transform: "rotate(-1deg)",
+        // border: "1px solid black",
+      },
+      
+}
+
+
+export const styleThree = {
+  bgContainer: {
+    position: "relative",
+    width: "600px",
+    height: "600px",
+    zIndex: "0",
+    backgroundImage: "url(/backgrounds/background3.png)",
+    backgroundSize: "cover",
+  },
+  printContainer: {
+    // marginTop: "500px",
+    width: "111px",
+    height: "157px",
+    position: "absolute",
+    top: "210px",
+    left:"307px"
+  },
+}
+
+
+export const styleFour = {
+  bgContainer: {
+    position: "relative",
+    width: "400px",
+    height: "620px",
+    zIndex: "0",
+    backgroundImage: "url(/backgrounds/background4.png)",
+    backgroundSize: "cover",
+  },
+  printContainer: {
+    position:'absolute',
+    width: "169px",
+    height : '263px',
+    top:'154px',
+    left:'102px',
+    // backgroundColor: 'blue',
+  },
+}
+
+
+
+  
